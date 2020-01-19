064dd8dbe71de049d8a61b14bc421f187e754171
Cédric Belin <cedric@belin.io>
2020-01-19 20:10:53 +0100

    Added the generated files for the code coverage

---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+export * from './json';
+export * from './simple_change';
+export * from './storage';
---
+++ lib/storage.d.ts
@@ -0,0 +1,85 @@
+import { JsonObject } from './json';
+/** Provides access to the [Web Storage](https://developer.mozilla.org/en-US/docs/Web/API/Storage). */
+export declare abstract class WebStorage extends EventTarget implements Iterable<[string, string | undefined]> {
+    private _backend;
+    /**
+     * An event that is triggered when a storage value is changed (added, modified, or removed).
+     * @event changes
+     */
+    static readonly eventChanges: string;
+    /** The function that listens for storage events. */
+    private readonly _listener;
+    /**
+     * Creates a new storage service.
+     * @param _backend The underlying data store.
+     */
+    protected constructor(_backend: Storage);
+    /** The keys of this storage. */
+    get keys(): string[];
+    /** The number of entries in this storage. */
+    get length(): number;
+    /**
+     * Returns a new iterator that allows iterating the entries of this storage.
+     * @return An iterator for the entries of this storage.
+     */
+    [Symbol.iterator](): IterableIterator<[string, string | undefined]>;
+    /** Removes all entries from this storage. */
+    clear(): void;
+    /** Cancels the subscription to the storage events. */
+    destroy(): void;
+    /**
+     * Gets the value associated to the specified key.
+     * @param key The key to seek for.
+     * @param defaultValue The value to return if the item does not exist.
+     * @return The value of the storage item, or the default value if the item is not found.
+     */
+    get(key: string, defaultValue?: string): string | undefined;
+    /**
+     * Gets the deserialized value associated to the specified key.
+     * @param key The key to seek for.
+     * @param defaultValue The value to return if the item does not exist.
+     * @return The deserialized value of the storage item, or the default value if the item is not found.
+     */
+    getObject(key: string, defaultValue?: any): any;
+    /**
+     * Gets a value indicating whether this storage contains the specified key.
+     * @param key The key to seek for.
+     * @return `true` if this storage contains the specified key, otherwise `false`.
+     */
+    has(key: string): boolean;
+    /**
+     * Removes the value associated to the specified key.
+     * @param key The key to seek for.
+     * @return The value associated with the specified key before it was removed.
+     */
+    remove(key: string): string | undefined;
+    /**
+     * Associates a given value to the specified key.
+     * @param key The key to seek for.
+     * @param value The item value.
+     * @return This instance.
+     */
+    set(key: string, value: string): this;
+    /**
+     * Serializes and associates a given value to the specified key.
+     * @param key The key to seek for.
+     * @param value The item value.
+     * @return This instance.
+     */
+    setObject(key: string, value: any): this;
+    /**
+     * Converts this object to a map in JSON format.
+     * @return The map in JSON format corresponding to this object.
+     */
+    toJSON(): JsonObject;
+}
+/** Provides access to the local storage. */
+export declare class LocalStorage extends WebStorage {
+    /** Creates a new storage service. */
+    constructor();
+}
+/** Provides access to the session storage. */
+export declare class SessionStorage extends WebStorage {
+    /** Creates a new storage service. */
+    constructor();
+}
