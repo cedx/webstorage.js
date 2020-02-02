4f2224a78f6d789477fa69698ed591dd3c3b86f4
Cédric Belin <cedric@belin.io>
2020-02-02 13:19:24 +0100

    * @return The value associated with the specified key.

--- src/storage.ts
+++ src/storage.ts
@@ -109,6 +109,7 @@ export abstract class WebStorage extends EventTarget implements Iterable<[string
    *
    * @param key The key to seek for.
    * @param ifAbsent The function called to get a new value.
+   * @return The value associated with the specified key.
    */
   putIfAbsent(key: string, ifAbsent: () => string): string {
     if (!this.has(key)) this.set(key, ifAbsent());
@@ -123,6 +124,7 @@ export abstract class WebStorage extends EventTarget implements Iterable<[string
    *
    * @param key The key to seek for.
    * @param ifAbsent The function called to get a new value.
+   * @return The value associated with the specified key.
    */
   putObjectIfAbsent(key: string, ifAbsent: () => any): any {
     if (!this.has(key)) this.setObject(key, ifAbsent());
