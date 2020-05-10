130de26ef67d3802d6e791e443a7fdc020fcdad3
Cédric Belin <cedric@belin.io>
2020-05-11 00:40:57 +0200

    Fixed the build system

--- tool/test.ps1
+++ tool/test.ps1
@@ -3,4 +3,4 @@ Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
 tool/build.ps1
-node_modules/.bin/karma.ps1 start etc/karma.cjs
+node_modules/.bin/karma start etc/karma.cjs
