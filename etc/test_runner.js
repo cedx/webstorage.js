54fcdebd04bf40a3450802fe71334d9ef8b105fb
Cédric Belin <cedric@belin.io>
2022-09-26 16:27:23 +0200

    Updated the tests

--- etc/test_runner.js
+++ etc/test_runner.js
@@ -1,8 +1,15 @@
+import {playwrightLauncher} from "@web/test-runner-playwright";
+
 /**
  * The test runner configuration.
  * @type {import("@web/test-runner").TestRunnerConfig}
  */
 export default {
+	browsers: [
+		playwrightLauncher({product: "chromium"}),
+		playwrightLauncher({product: "firefox"}),
+		playwrightLauncher({product: "webkit"})
+	],
 	coverage: true,
 	coverageConfig: {reportDir: "var"},
 	files: "test/**/*.js",
