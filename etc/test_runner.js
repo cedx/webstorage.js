35c70f418c971df8b83f36e4df40ae2f97e1cb64
Cédric Belin <cedric@belin.io>
2024-06-07 15:39:49 +0200

    Code formatting [skip ci]

--- etc/test_runner.js
+++ etc/test_runner.js
@@ -1,14 +1,11 @@
 import {playwrightLauncher} from "@web/test-runner-playwright";
 
-/**
- * @type {import("@web/test-runner").TestRunnerConfig}
- */
 export default {
+	files: "test/**/*.js",
+	nodeResolve: true,
 	browsers: [
 		playwrightLauncher({product: "chromium"}),
 		playwrightLauncher({product: "firefox"}),
 		playwrightLauncher({product: "webkit"})
-	],
-	files: "test/**/*.js",
-	nodeResolve: true
+	]
 };
