776fa59cb7bbc2f86ba209dae7e1f8966f5a84f3
Cédric Belin <cedric@belin.io>
2024-10-27 14:57:52 +0100

    Improve code completion [skip ci]

--- etc/rollup.js
+++ etc/rollup.js
@@ -1,5 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
 
+/** @type {import("rollup").RollupOptions} */
 export default {
 	input: "test/index.js",
 	output: {file: "var/tests.js", format: "iife"},
