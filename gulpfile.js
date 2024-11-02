883e77261e841b76570579ca4599caf4df1a8f61
Cédric Belin <cedric@belin.io>
2024-11-02 23:51:00 +0100

    Rename the `exe()` function to `npx()` [skip ci]

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ import pkg from "./package.json" with {type: "json"};
 
 /** Builds the project. */
 export function build() {
-	return exec("tsc", "--build", "src/tsconfig.json");
+	return npx("tsc", "--build", "src/tsconfig.json");
 }
 
 /** Deletes all generated files. */
@@ -18,8 +18,8 @@ export async function clean() {
 /** Performs the static analysis of source code. */
 export async function lint() {
 	await build();
-	await exec("tsc", "--build", "tsconfig.json");
-	return exec("eslint", "--config=etc/eslint.js", "gulpfile.js", "example", "src", "test");
+	await npx("tsc", "--build", "tsconfig.json");
+	return npx("eslint", "--config=etc/eslint.js", "gulpfile.js", "example", "src", "test");
 }
 
 /** Publishes the package. */
@@ -31,13 +31,13 @@ export async function publish() {
 /** Runs the test suite. */
 export async function test() {
 	await build();
-	await exec("rollup", "--config=etc/rollup.js");
+	await npx("rollup", "--config=etc/rollup.js");
 	return run("node", "etc/puppeteer.js");
 }
 
 /** Watches for file changes. */
 export function watch() {
-	return exec("tsc", "--build", "src/tsconfig.json", "--preserveWatchOutput", "--watch");
+	return npx("tsc", "--build", "src/tsconfig.json", "--preserveWatchOutput", "--watch");
 }
 
 /** The default task. */
