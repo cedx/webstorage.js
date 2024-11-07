3401f061c1ac2b59766db75053a20048365d20a0
Cédric Belin <cedric@belin.io>
2024-11-07 01:27:42 +0100

    Port the `StorageEvent` class

--- lib/storage_event.d.ts
+++ lib/storage_event.d.ts
@@ -1,29 +1,33 @@
 /**
  * An event dispatched when the storage has been changed.
  */
-export declare class StorageEvent extends Event {
-    /**
-     * The event type.
-     */
-    static readonly type = "storage:change";
-    /**
-     * The changed key.
-     */
-    readonly key: string | null;
-    /**
-     * The new value.
-     */
-    readonly newValue: string | null;
-    /**
-     * The original value.
-     */
-    readonly oldValue: string | null;
-    /**
-     * Creates a new cookie event.
-     * @param key The changed key.
-     * @param oldValue The original value.
-     * @param newValue The new value.
-     */
-    constructor(key: string | null, oldValue?: string | null, newValue?: string | null);
+export class StorageEvent extends Event {
+
+	/**
+	 * The event type.
+	 */
+	static readonly type: string;
+
+	/**
+	 * The changed key.
+	 */
+	key: string|null;
+
+	/**
+	 * The new value.
+	 */
+	newValue: string|null;
+
+	/**
+	 * The original value.
+	 */
+	oldValue: string|null;
+
+	/**
+	 * Creates a new storage event.
+	 * @param key The changed key.
+	 * @param oldValue The original value.
+	 * @param newValue The new value.
+	 */
+	constructor(key: string|null, oldValue?: string|null, newValue?: string|null);
 }
-//# sourceMappingURL=storage_event.d.ts.map
