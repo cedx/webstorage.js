11cfb085cea222739112977c556df80168347d24
Cédric Belin <cedric@belin.io>
2024-11-07 10:30:39 +0100

    Port the examples

--- example/iteration.js
+++ example/iteration.js
@@ -7,23 +7,17 @@ const localStorage = Storage.local()
 	.set("bar", "baz")
 	.set("baz", "qux");
 
-for (const [key, value] of localStorage) {
-	console.log(`${key} => ${value}`);
-	// Round 1: "foo => bar"
-	// Round 2: "bar => baz"
-	// Round 3: "baz => qux"
-}
+for (const [key, value] of localStorage) console.log(`${key} => ${value}`);
+// Round 1: "foo => bar"
+// Round 2: "bar => baz"
+// Round 3: "baz => qux"
 
 // Loop over entries of the session storage that use the same key prefix.
 Storage.session()
 	.set("foo", "bar")
 	.set("prefix:bar", "baz");
 
-const prefixedStorage = Storage.session({keyPrefix: "prefix:"})
-	.set("baz", "qux");
-
-for (const [key, value] of prefixedStorage) {
-	console.log(`${key} => ${value}`);
-	// Round 1: "bar => baz"
-	// Round 2: "baz => qux"
-}
+const prefixedStorage = Storage.session({keyPrefix: "prefix:"}).set("baz", "qux");
+for (const [key, value] of prefixedStorage) console.log(`${key} => ${value}`);
+// Round 1: "bar => baz"
+// Round 2: "baz => qux"
