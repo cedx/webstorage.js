f0579edf00ff523ae24541836b1b8e41dd26e54e
Cédric Belin <cedric.belin@mc2it.com>
2025-03-17 11:18:34 +0100

    Update the settings of the static analyzer [skip ci]

--- etc/eslint.js
+++ etc/eslint.js
@@ -143,7 +143,7 @@ export default ts.config(
 			"prefer-template": "error",
 			"radix": ["error", "as-needed"],
 			"require-atomic-updates": ["error", {allowProperties: true}],
-			"require-await": "off",
+			"require-await": "error",
 			"require-unicode-regexp": "off",
 			"sort-imports": "off",
 			"sort-keys": "off",
