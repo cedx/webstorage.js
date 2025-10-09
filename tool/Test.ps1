6272db376752a2cfc56d5b92899d6e2292518557
Cédric Belin <cedx@outlook.com>
2025-10-09 02:44:42 +0200

    Update the "Lint" script [skip ci]

--- tool/Test.ps1
+++ tool/Test.ps1
@@ -1,4 +1,4 @@
-Write-Host "Running the test suite..."
+Write-Output "Running the test suite..."
 npx tsc --build src/tsconfig.json --sourceMap
 npx esbuild test/Main.js --bundle --legal-comments=none --log-level=warning --outfile=var/Tests.js
 node test/Playwright.js
