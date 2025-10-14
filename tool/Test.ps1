a85bbe37140f11c664dd3e2b5e21fbb134cede74
Cédric Belin <cedx@outlook.com>
2025-10-14 08:26:45 +0200

    Code optimization [skip ci]

--- tool/Test.ps1
+++ tool/Test.ps1
@@ -1,4 +1,4 @@
-Write-Output "Running the test suite..."
+"Running the test suite..."
 npx tsc --build src/tsconfig.json --sourceMap
 npx esbuild test/Main.js --bundle --legal-comments=none --log-level=warning --outfile=var/Tests.js --sourcemap
 node test/Playwright.js
