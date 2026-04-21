96ad0d58aae1592498129a71668acc0ec3292ee8
Cédric Belin <cedx@outlook.com>
2026-04-22 00:26:28 +0200

    Update the build system [skip ci]

--- tool/Test.ps1
+++ tool/Test.ps1
@@ -1,4 +1,5 @@
+using module ./Cmdlets.psm1
+
 "Running the test suite..."
-npx tsc --build src/tsconfig.json --sourceMap
-npx esbuild test/Main.js --bundle --legal-comments=none --log-level=warning --outfile=var/Tests.js --sourcemap
-node test/Playwright.js
+Invoke-TypeScript "$PSScriptRoot/../src/tsconfig.json" -SourceMap
+Invoke-NodeTesting
